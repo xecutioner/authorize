8a2a7a28003b87df510337a872858aac1dfc17aa
kapil <kapil@esignature.com.np>
2013-02-11 10:49:16 +0545

    initial payload

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Authorize::Application.config.secret_token = '2b5598500936c83c617352a908ebc9088b0aca44df6043824025775df6c50bb8a1bed4aec662c16cff7ed288320563d2639b49e9d27b7a85a0b0311c22215740'
